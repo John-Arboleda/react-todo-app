a5519afcb839618a25f8013af96925f24ac51f8f
John-Arboleda <johnjack0126@gmail.com>
2022-04-26 08:01:18 -0500

    Enable checkbox state update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./components/TodoContainer"
+/* eslint-disable-next-line no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* eslint-disable-next-line no-unused-vars */
+import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById("root")
-)
+  document.getElementById('root'),
+);
